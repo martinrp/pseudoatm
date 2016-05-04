8dd51a6ee95040bf01ef345362826618734437c5
martinrp <martin.ri.p@gmail.com>
2016-05-04 15:21:55 +0200

    Add pin route to Hello microapp and fix CSS bug

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // @see: https://gist.github.com/branneman/8048520
 require('app-module-path').addPath(__dirname + '/lib');
 
-var server = require('nodebootstrap-server')
-  , appConfig = require('./appConfig')
-  , app    = require('express')();
+var server = require('nodebootstrap-server'),
+    appConfig = require('./appConfig'),
+    app    = require('express')();
 
 
 app = require('nodebootstrap-htmlapp').setup(app);
